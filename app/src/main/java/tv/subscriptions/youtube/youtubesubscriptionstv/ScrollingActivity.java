bd8e32d91f51fb3e5b0d9ea507468b63602d0b32
blablanumerodeux <blablanumerodeux@gmail.com>
2016-10-29 20:53:03 +0200

    added OAuth2 support

--- app/src/main/java/tv/subscriptions/youtube/youtubesubscriptionstv/ScrollingActivity.java
+++ app/src/main/java/tv/subscriptions/youtube/youtubesubscriptionstv/ScrollingActivity.java
@@ -1,26 +1,53 @@
 package tv.subscriptions.youtube.youtubesubscriptionstv;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import net.openid.appauth.AuthState;
+import net.openid.appauth.AuthorizationException;
+import net.openid.appauth.AuthorizationRequest;
+import net.openid.appauth.AuthorizationResponse;
+import net.openid.appauth.AuthorizationService;
+import net.openid.appauth.AuthorizationServiceConfiguration;
+import net.openid.appauth.TokenResponse;
+
+import org.json.JSONException;
 
 
 public class ScrollingActivity extends AppCompatActivity {
 
     private String finalUrl="https://www.youtube.com/feeds/videos.xml?channel_id=UCNu6L-xcmBsN3vNFOxYvB2Q";
     private HandleXML obj;
+    AppCompatButton mAuthorize;
+    private static final String SHARED_PREFERENCES_NAME = "AuthStatePreference";
+    private static final String AUTH_STATE = "AUTH_STATE";
+    private static final String USED_INTENT = "USED_INTENT";
+    // state
+    AuthState mAuthState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mAuthorize = (AppCompatButton) findViewById(R.id.authorize);
+        mAuthorize.setOnClickListener(new AuthorizeListener());
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -58,4 +85,173 @@ public class ScrollingActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
+    /**
+     * Kicks off the authorization flow.
+     */
+    public static class AuthorizeListener implements Button.OnClickListener {
+        @Override
+        public void onClick(View view) {
+
+            // code from the step 'Create the Authorization Request',
+            // and the step 'Perform the Authorization Request' goes here.
+            AuthorizationServiceConfiguration serviceConfiguration = new AuthorizationServiceConfiguration(
+                    Uri.parse("https://accounts.google.com/o/oauth2/auth") /* auth endpoint */,
+                    Uri.parse("https://accounts.google.com/o/oauth2/token") /* token endpoint */
+            );
+
+
+
+            String clientId = "99998132999-okeq5mdnjj3uv6q4kape9emq0eeg92ge.apps.googleusercontent.com";
+            Uri redirectUri = Uri.parse("tv.subscriptions.youtube.youtubesubscriptionstv:/oauth2redirect");
+            AuthorizationRequest.Builder builder = new AuthorizationRequest.Builder(
+                    serviceConfiguration,
+                    clientId,
+                    AuthorizationRequest.RESPONSE_TYPE_CODE,
+                    redirectUri
+            );
+            builder.setScopes("profile");
+            AuthorizationRequest request = builder.build();
+
+
+
+            AuthorizationService authorizationService = new AuthorizationService(view.getContext());
+
+
+            String action = "tv.subscriptions.youtube.youtubesubscriptionstv.HANDLE_AUTHORIZATION_RESPONSE";
+            Intent postAuthorizationIntent = new Intent(action);
+            PendingIntent pendingIntent = PendingIntent.getActivity(view.getContext(), request.hashCode(), postAuthorizationIntent, 0);
+            authorizationService.performAuthorizationRequest(request, pendingIntent);
+
+
+        }
+    }
+
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        checkIntent(intent);
+    }
+
+    private void checkIntent(@Nullable Intent intent) {
+        if (intent != null) {
+            String action = intent.getAction();
+            switch (action) {
+                case "tv.subscriptions.youtube.youtubesubscriptionstv.HANDLE_AUTHORIZATION_RESPONSE":
+                    if (!intent.hasExtra(USED_INTENT)) {
+                        handleAuthorizationResponse(intent);
+                        intent.putExtra(USED_INTENT, true);
+                    }
+                    break;
+                default:
+                    // do nothing
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkIntent(getIntent());
+    }
+
+
+
+
+    /**
+     * Exchanges the code, for the {@link TokenResponse}.
+     *
+     * @param intent represents the {@link Intent} from the Custom Tabs or the System Browser.
+     */
+    private void handleAuthorizationResponse(@NonNull Intent intent) {
+
+        // code from the step 'Handle the Authorization Response' goes here.
+        AuthorizationResponse response = AuthorizationResponse.fromIntent(intent);
+        AuthorizationException error = AuthorizationException.fromIntent(intent);
+        final AuthState authState = new AuthState(response, error);
+        final String LOG_TAG = "AppAuthSample";
+
+        if (response != null) {
+            Log.i(LOG_TAG, String.format("Handled Authorization Response %s ", authState.toJsonString()));
+            AuthorizationService service = new AuthorizationService(this);
+            service.performTokenRequest(response.createTokenExchangeRequest(), new AuthorizationService.TokenResponseCallback() {
+                @Override
+                public void onTokenRequestCompleted(@Nullable TokenResponse tokenResponse, @Nullable AuthorizationException exception) {
+                    if (exception != null) {
+                        Log.w(LOG_TAG, "Token Exchange failed", exception);
+                    } else {
+                        if (tokenResponse != null) {
+                            authState.update(tokenResponse, exception);
+                            persistAuthState(authState);
+                            Log.i(LOG_TAG, String.format("Token Response [ Access Token: %s, ID Token: %s ]", tokenResponse.accessToken, tokenResponse.idToken));
+                        }
+                    }
+                }
+            });
+        }
+
+    }
+
+    private void persistAuthState(@NonNull AuthState authState) {
+        getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE).edit()
+                .putString(AUTH_STATE, authState.toJsonString())
+                .commit();
+        enablePostAuthorizationFlows();
+    }
+
+
+
+    private void enablePostAuthorizationFlows() {
+        mAuthState = restoreAuthState();
+        if (mAuthState != null && mAuthState.isAuthorized()) {
+            new MakeApiCallListener(this, mAuthState, new AuthorizationService(this));
+        }
+    }
+
+
+
+    @Nullable
+    private AuthState restoreAuthState() {
+        String jsonString = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+                .getString(AUTH_STATE, null);
+        if (!TextUtils.isEmpty(jsonString)) {
+            try {
+                return AuthState.fromJson(jsonString);
+            } catch (JSONException jsonException) {
+                // should never happen
+            }
+        }
+        return null;
+    }
+
+
+
+
+
+    public static class MakeApiCallListener implements Button.OnClickListener {
+
+        private final ScrollingActivity mMainActivity;
+        private AuthState mAuthState;
+        private AuthorizationService mAuthorizationService;
+
+        public MakeApiCallListener(@NonNull ScrollingActivity mainActivity, @NonNull AuthState authState, @NonNull AuthorizationService authorizationService) {
+            mMainActivity = mainActivity;
+            mAuthState = authState;
+            mAuthorizationService = authorizationService;
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            // code from the section 'Making API Calls' goes here
+
+        }
+    }
+
+
+
 }
